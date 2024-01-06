f2b616a214803ac4f3be91098304ab389cd3ab9c
Serdar Tufan <serdartufan4@gmail.com>
2024-01-06 17:22:41 +0300

    GameBoard Initialization and Swap Mechanic

--- Assets/Scripts/Gameplay/IconSwappable.cs
+++ Assets/Scripts/Gameplay/IconSwappable.cs
@@ -5,29 +5,40 @@ using UnityEngine.UI;
 
 public class IconSwappable : MonoBehaviour
 {
-    int iconIndex;
-    Sprite unselectedImage;
-    Sprite selectedImage;
+    public int IconIndex { get; private set; }
+    public Sprite UnselectedImage { get; private set; }
+    public Sprite SelectedImage { get; private set; }
     bool isSelected = false;
 
     public void ToggleSelection()
     {
         if (isSelected)
         {
-            gameObject.GetComponent<Image>().sprite = unselectedImage;
+            gameObject.GetComponent<Image>().sprite = UnselectedImage;
             isSelected = false;
         }
         else
         {
-            gameObject.GetComponent<Image>().sprite = selectedImage;
+            gameObject.GetComponent<Image>().sprite = SelectedImage;
             isSelected = true;
         }
     }
 
     public void SetupIconElement(int iconIndex,Sprite unselectedImage, Sprite selectedImage)
     {
-        this.iconIndex = iconIndex;
-        this.unselectedImage = unselectedImage;
-        this.selectedImage = selectedImage;
+        this.IconIndex = iconIndex;
+        this.UnselectedImage = unselectedImage;
+        this.SelectedImage = selectedImage;
+    }
+
+    public void InitializeIconElement()
+    {
+        gameObject.GetComponent<Image>().sprite = UnselectedImage;
+        isSelected = false;
+    }
+
+    public void InitIconElement()
+    {
+        this.IconIndex = -1;
     }
 }
--- Assets/Scripts/Gameplay/IconSwapper.cs
+++ Assets/Scripts/Gameplay/IconSwapper.cs
@@ -5,20 +5,76 @@ using UnityEngine.UI;
 
 public class IconSwapper : MonoBehaviour
 {
+    [SerializeField] private List<GameObject> horizontalParentObjectList;
     public void Swap(IconSwappable icon1,IconSwappable icon2)
     {
-        //parentObject.transform.GetChild(0).gameObject.GetComponent<HorizontalLayoutGroup>().enabled = false;
-        Vector3 posIcon1 = icon1.gameObject.transform.position;
-        Vector3 posIcon2 = icon2.gameObject.transform.position;
+        foreach (GameObject child in horizontalParentObjectList)
+        {
+            child.transform.GetChild(0).gameObject.GetComponent<HorizontalLayoutGroup>().enabled = false;
+        }
 
-        icon1.gameObject.transform.position = posIcon2;
-        icon2.gameObject.transform.position = posIcon1;
+        //Vector3 posIcon1 = icon1.gameObject.transform.position;
+        //Vector3 posIcon2 = icon2.gameObject.transform.position;
+
+        //icon1.gameObject.transform.position = posIcon2;
+        //icon2.gameObject.transform.position = posIcon1;
+
+
+        #region SwapHorizontalElements
+        //IconSwappable gameobject'inin bir ara parent'ý var horizontal layout grouptan etkilenen bu parent
+        Transform icon1HorizontalElement = icon1.gameObject.transform.parent.transform;
+        Transform icon2HorizontalElement = icon2.gameObject.transform.parent.transform;
+
+        //Horizontal Layout Group içeren parentlarý al
+        Transform parentOfIcon1 = icon1HorizontalElement.parent;
+        Transform parentOfIcon2 = icon2HorizontalElement.parent;
+
+        //Child indexlerini bul
+        int childIndexOfIcon1 = FindChildIndex(parentOfIcon1, icon1HorizontalElement.gameObject);
+        int childIndexOfIcon2 = FindChildIndex(parentOfIcon2, icon2HorizontalElement.gameObject);
+
+        // Icon'larýn parent object'lerini swapla
+        icon2HorizontalElement.transform.SetParent(parentOfIcon1);
+        icon1HorizontalElement.transform.SetParent(parentOfIcon2);
+
+        // Icon'larýn child index'lerini swapla
+        icon2HorizontalElement.transform.SetSiblingIndex(childIndexOfIcon1);
+        icon1HorizontalElement.transform.SetSiblingIndex(childIndexOfIcon2);
+        #endregion
+
+        foreach (GameObject child in horizontalParentObjectList)
+        {
+            child.transform.GetChild(0).gameObject.GetComponent<HorizontalLayoutGroup>().enabled = true;
+        }
+
+
     }
 
     public bool IsValidSwap(IconSwappable icon1, IconSwappable icon2)
     {
-        bool isValid = false;
+        bool isValid = true;
 
         return isValid;
     }
+
+    public int FindChildIndex(Transform parent,GameObject targetObject)
+    {
+        int childIndex = -1;
+        int childCount = parent.childCount;
+
+        // Child'ýn index'ini bulmak için parent'ýn childlarýný dönün
+        for (int i = 0; i < childCount; i++)
+        {
+            if (parent.GetChild(i) == targetObject.transform)
+            {
+                // Ýstenen child bulundu, i deðeri child'ýn index'ini temsil ediyor
+                childIndex = i;
+                return childIndex;
+            }
+        }
+
+        return childIndex;
+    }
+
+    
 }
